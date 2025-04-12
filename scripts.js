1717161d781247b0fba49607fb6c5c010235b128
Henry Khine <hwkhine@gmail.com>
2025-04-11 21:00:08 -0700

    push

--- scripts.js
+++ scripts.js
@@ -44,7 +44,78 @@ let URL = [
   "https://www.deckshop.pro/img/card_ed/Bandit.png",
   "https://www.deckshop.pro/img/card_ed/Fisherman.png",
   "https://www.deckshop.pro/img/card_ed/LittlePrince.png",
-
+  "https://www.deckshop.pro/img/card_ed/SkeletonDragons.png",
+  "https://www.deckshop.pro/img/card_ed/Mortar.png",
+  "https://www.deckshop.pro/img/card_ed/Tesla.png",
+  "https://www.deckshop.pro/img/card_ed/Fireball.png",
+  "https://www.deckshop.pro/img/card_ed/MP.png",
+  "https://www.deckshop.pro/img/card_ed/Musk.png",
+  "https://www.deckshop.pro/img/card_ed/Valk.png",
+  "https://www.deckshop.pro/img/card_ed/GoblinCage.png",
+  "https://www.deckshop.pro/img/card_ed/Ram.png",
+  "https://www.deckshop.pro/img/card_ed/BombTower.png",
+  "https://www.deckshop.pro/img/card_ed/FlyingMachine.png",
+  "https://www.deckshop.pro/img/card_ed/Hog.png",
+  "https://www.deckshop.pro/img/card_ed/BattleHealer.png",
+  "https://www.deckshop.pro/img/card_ed/Furnace.png",
+  "https://www.deckshop.pro/img/card_ed/Zappies.png",
+  "https://www.deckshop.pro/img/card_ed/GoblinDemolisher.png",
+  "https://www.deckshop.pro/img/card_ed/BabyD.png",
+  "https://www.deckshop.pro/img/card_ed/DarkPrince.png",
+  "https://www.deckshop.pro/img/card_ed/Freeze.png",
+  "https://www.deckshop.pro/img/card_ed/Poison.png",
+  "https://www.deckshop.pro/img/card_ed/RuneGiant.png",
+  "https://www.deckshop.pro/img/card_ed/Hunter.png",
+  "https://www.deckshop.pro/img/card_ed/GoblinDrill.png",
+  "https://www.deckshop.pro/img/card_ed/eWiz.png",
+  "https://www.deckshop.pro/img/card_ed/InfernoD.png",
+  "https://www.deckshop.pro/img/card_ed/Phoenix.png",
+  "https://www.deckshop.pro/img/card_ed/MagicArcher.png",
+  "https://www.deckshop.pro/img/card_ed/Lumber.png",
+  "https://www.deckshop.pro/img/card_ed/NightWitch.png",
+  "https://www.deckshop.pro/img/card_ed/MotherWitch.png",
+  "https://www.deckshop.pro/img/card_ed/GoldenKnight.png",
+  "https://www.deckshop.pro/img/card_ed/SkeletonKing.png",
+  "https://www.deckshop.pro/img/card_ed/MightyMiner.png",
+  "https://www.deckshop.pro/img/card_ed/Barbs.png",
+  "https://www.deckshop.pro/img/card_ed/Horde.png",
+  "https://www.deckshop.pro/img/card_ed/Rascals.png",
+  "https://www.deckshop.pro/img/card_ed/Giant.png",
+  "https://www.deckshop.pro/img/card_ed/GobHut.png",
+  "https://www.deckshop.pro/img/card_ed/Inferno.png",
+  "https://www.deckshop.pro/img/card_ed/Wiz.png",
+  "https://www.deckshop.pro/img/card_ed/RoyalHogs.png",
+  "https://www.deckshop.pro/img/card_ed/Witch.png",
+  "https://www.deckshop.pro/img/card_ed/Balloon.png",
+  "https://www.deckshop.pro/img/card_ed/Prince.png",
+  "https://www.deckshop.pro/img/card_ed/eDragon.png",
+  "https://www.deckshop.pro/img/card_ed/Bowler.png",
+  "https://www.deckshop.pro/img/card_ed/Exe.png",
+  "https://www.deckshop.pro/img/card_ed/CannonCart.png",
+  "https://www.deckshop.pro/img/card_ed/RamRider.png",
+  "https://www.deckshop.pro/img/card_ed/Graveyard.png",
+  "https://www.deckshop.pro/img/card_ed/GoblinMachine.png",
+  "https://www.deckshop.pro/img/card_ed/ArcherQueen.png",
+  "https://www.deckshop.pro/img/card_ed/Goblinstein.png",
+  "https://www.deckshop.pro/img/card_ed/Monk.png",
+  "https://www.deckshop.pro/img/card_ed/RG.png",
+  "https://www.deckshop.pro/img/card_ed/eBarbs.png",
+  "https://www.deckshop.pro/img/card_ed/Rocket.png",
+  "https://www.deckshop.pro/img/card_ed/BarbHut.png",
+  "https://www.deckshop.pro/img/card_ed/Pump.png",
+  "https://www.deckshop.pro/img/card_ed/GiantSkelly.png",
+  "https://www.deckshop.pro/img/card_ed/Lightning.png",
+  "https://www.deckshop.pro/img/card_ed/GobGiant.png",
+  "https://www.deckshop.pro/img/card_ed/XBow.png",
+  "https://www.deckshop.pro/img/card_ed/Sparky.png",
+  "https://www.deckshop.pro/img/card_ed/BossBandit.png",
+  "https://www.deckshop.pro/img/card_ed/RoyalRecruits.png",
+  "https://www.deckshop.pro/img/card_ed/PEKKA.png",
+  "https://www.deckshop.pro/img/card_ed/ElectroGiant.png",
+  "https://www.deckshop.pro/img/card_ed/MegaKnight.png",
+  "https://www.deckshop.pro/img/card_ed/Lava.png",
+  "https://www.deckshop.pro/img/card_ed/Golem.png",
+  "https://www.deckshop.pro/img/card_ed/3M.png"
   
 ];
 
@@ -93,8 +164,79 @@ let titles = [
     "Royal Ghost",
     "Bandit",
     "Fisherman",
-    "Little Prince"
-  
+    "Little Prince",
+    "Skeleton Dragons",
+    "Mortar",
+    "Tesla",
+    "Fireball",
+    "Mini Pekka",
+    "Musketeer",
+    "Valkyrie",
+    "Goblin Cage",
+    "Battle Ram",
+    "Bomb Tower",
+    "Flying Macine",
+    "Hog Rider",
+    "Battle Healer",
+    "Furnace",
+    "Zappies",
+    "Goblin Demolisher",
+    "Baby Dragon",
+    "Dark Prince",
+    "Freeze",
+    "Poison",
+    "Rune Giant",
+    "Hunter",
+    "Goblin Drill",
+    "Electro Wizard",
+    "Inferno Dragon",
+    "Phoenix",
+    "Magic Archer",
+    "Lumberjack",
+    "Night Witch",
+    "Mother Witch",
+    "Golden Knight",
+    "Skeleton King",
+    "Mighty Miner",
+    "Barbarians",
+    "Minion Horde",
+    "Rascals",
+    "Giant",
+    "Goblin Hut",
+    "Inferno Tower",
+    "Wizard",
+    "Royal Hogs",
+    "Witch",
+    "Balloon",
+    "Prince",
+    "Electro Dragon",
+    "Bowler",
+    "Executioner",
+    "Cannon Cart",
+    "Ram Rider",
+    "Graveyard",
+    "Goblin Machine",
+    "Archer Queen",
+    "Goblinstein",
+    "Monk",
+    "Royal Giant",
+    "Elite Barbarians",
+    "Rocket",
+    "Barbarian Hut",
+    "Elixir Collector",
+    "Giant Skeleton",
+    "Lightning",
+    "Goblin Giant",
+    "X-Bow",
+    "Sparky",
+    "Boss Bandit",
+    "Royal Recruits",
+    "Pekka",
+    "Electro Giant",
+    "Mega Knight",
+    "Lavahound",
+    "Golem",
+    "Three Musketeers"
 ];
 
 let bulletPoints = [
@@ -142,7 +284,80 @@ let bulletPoints = [
   ["1x Royal Ghost", "Ground Unit", "Melee-Splash"],
   ["1x Bandit", "Ground Unit", "Melee-Single"],
   ["1x Fisherman", "Ground Unit", "Melee-Single"],
-  ["1x Little Prince", "Ground Unit", "Ranged-Single"]
+  ["1x Little Prince", "Ground Unit", "Ranged-Single"],
+  ["2x Skeleton Dragons", "Flying", "Ranged-Splash"],
+  ["1x Mortar", "Building", "Tower-Seige"],
+  ["1x Tesla", "Building", "Tower-Single"],
+  ["1x Fireball", "Spell", "Splash"],
+  ["1x Mini Pekka", "Ground Unit", "Melee-Single"],
+  ["1x Musketeer", "Ground Unit", "Ranged-Single"],
+  ["1x Valkyrie", "Ground Unit", "Melee-Splash"],
+  ["1x Goblin Cage", "Tower", "Spawner"],
+  ["2x Barbarians", "Ground Unit", "Towers"],
+  ["1x Bomb Tower", "Building", "Splash"],
+  ["1x Flying Machine", "Flying", "Ranged-Single"],
+  ["1x Hog Rider", "Ground Unit", "Towers"],
+  ["1x Battle Healer", "Ground Unit", "Melee-Single"],
+  ["1x Furnace", "Tower", "Spawner"],
+  ["3x Zappies", "Ground Unit", "Ranged-Single"],
+  ["1x Goblin Demolisher", "Ground Unit", "Ranged-Splash"],
+  ["1x Baby Dragon", "Flying", "Ranged-Splash"],
+  ["1x Dark Prince", "Ground Unit", "Melee-Splash"],
+  ["1x Freeze", "Spell", "Freezes"],
+  ["1x Poison", "Spell", "Splash"],
+  ["1x Rune Giant", "Ground Unit", "Towers"],
+  ["1x Hunter", "Ground Unit", "Ranged-Single"],
+  ["1x Goblin Drill", "Spell", "Spawner"],
+  ["1x Electro Wizard", "Ground Unit", "Ranged-Double"],
+  ["1x Inferno Dragon", "Flying", "Ranged-Single"],
+  ["1x Phoenix", "Flying", "Ranged-Single"],
+  ["1x Magic Archer", "Ground Unit", "Ranged-Piercing"],
+  ["1x Lumberjack", "Ground Unit", "Melee-Single, Rage"],
+  ["1x Night Witch", "Ground Unit", "Melee-Single + Spawns Bats"],
+  ["1x Mother Witch", "Ground Unit", "Range-Single + Spawns Pigs"],
+  ["1x Golden Knight", "Ground Unit", "Melee-Single, Dash"],
+  ["1x Skeleton King", "Ground Unit", "Melee-Single + Spawns Skeletons"],
+  ["1x Mighty Miner", "Ground Unit", "Melee-Single, Tunnel"],
+  ["5x Barbarians", "Ground Unit", "Melee-Single"],
+  ["6x Minions", "Flying", "Ranged-Single"],
+  ["2x Boy Rascal + 1x Girl Rascal", "Ground Unit", "Mixed-Single"],
+  ["1x Giant", "Ground Unit", "Towers"],
+  ["1x Goblin Hut", "Tower", "Spawner"],
+  ["1x Inferno Tower", "Tower", "Single"],
+  ["1x Wizard", "Ground Unit", "Ranged-Splash"],
+  ["5x Royal Hogs", "Ground Unit", "Tower"],
+  ["1x Witch", "Groud Unit", "Ranged-Splash"],
+  ["1x Balloon", "Flying", "Towers"],
+  ["1x Prince", "Ground Unit", "Melee-Single"],
+  ["1x Electro Dragon", "Flying", "Ranged-Chain"],
+  ["1x Bowler", "Ground Unit", "Ranged-Splash"],
+  ["1x Executioner", "Ground Unit", "Range-Splash"],
+  ["1x Cannon Cart", "Ground Unit", "Range-Single"],
+  ["1x Ram Rider", "Ground Unit", "Melee-Single/Towers"],
+  ["1x Graveyard", "Spell", "Spawn Skeletons"],
+  ["1x Goblin Machine", "Ground Unit", "Towers"],
+  ["1x Archer Queen", "Ground Unit", "Range-Single"],
+  ["1x Goblinstein", "Ground Unit", "Range-Single"],
+  ["1x Monk", "Ground Unit", "Melee-Single"],
+  ["1x Royal Giant", "Ground Unit", "Tower"],
+  ["2x Elite Barbarian", "Ground Unit", "Melee-Single"],
+  ["1x Rocket", "Spell", "Splash"],
+  ["1x Barbarian Hut", "Tower", "Spawner"],
+  ["1x Royal Giant", "Ground Unit", "Tower"],
+  ["1x Giant Skeleton", "Ground Unit", "Melee-Single"],
+  ["3x Lightning", "Spell", "Single"],
+  ["1x Goblin Giant + 2x Spear Goblin", "Ground Unit", "Tower"],
+  ["1x X-Bow", "Tower", "Seige"],
+  ["1x Sparky", "Ground Unit", "Range-Splash"],
+  ["1x Boss Bandit", "Ground Unit", "Melee-Single + Dash"],
+  ["6x Recruits", "Ground Unit", "Melee-Single"],
+  ["1x PEKKA", "Ground Unit", "Melee-Single"],
+  ["1x Electro Giant", "Ground Unit", "Tower"],
+  ["1x Mega Knight", "Ground Unit", "Melee-Splash"],
+  ["1x Lavahound", "Flying", "Tower"],
+  ["1x Golem", "Ground Unit", "Tower"],
+  ["3x Musketeers", "Ground Unit", "Range-Single"]
+  
 ];
 
 let elixerCosts = [
@@ -190,22 +405,244 @@ let elixerCosts = [
   3, // Royal Ghost
   3, // Bandit
   3, // Fisherman
-  3  // Little Prince (assuming he’s a champion-type card)
+  3,  // Little Prince
+  4, // Skeleton Dragons
+  4, // Mortar
+  4, // Tesla
+  4, // Fireball
+  4, // Mini Pekka
+  4, // Musketeer
+  4, // Goblin Cage
+  4, // Valkyrie
+  4,  // Battle Ram
+  4, //"Bomb Tower",
+  4, //  "Flying Macine",
+  4,//    "Hog Rider",
+  4,//  "Battle Healer",
+  4,//    "Furnace",
+  4,//    "Zappies",
+  4,//    "Goblin Demolisher",
+  4,//    "Baby Dragon",
+  4,//    "Dark Prince",
+  4,//    "Freeze",
+  4,//    "Poison",
+  4,//    "Rune Giant",
+  4,//    "Hunter",
+  4,//    "Goblin Drill",
+  4,//    "Electro Wizard",
+  4,//    "Inferno Dragon",
+  4,//    "Phoenix",
+  4,//    "Magic Archer"
+  4,// Lumberjack
+  4,// Night Witch
+  4,// Mother Witch
+  4,// Golden Knight
+  4,// Skeleton King
+  4,// Mighty Miner
+  5, // Barbarians
+  5, // Minion Horde
+  5, // Rascals
+  5, // Giant
+  5, // Goblin Hut
+  5, // Inferno Tower
+  5, // Wizard
+  5, // Royal Hogs
+  5,  // Witch
+  5, // Balloon
+  5, // Prince
+  5, // Electro Dragon
+  5, // Bowler
+  5, // Executioner
+  5, // Cannon Cart
+  5, // Ram Rider
+  5, // Graveyard
+  5,  // Goblin Giant
+  5,
+  5,
+  5,
+  6, // Royal Giant
+  6, // Elite Barbarians
+  6, // Rocket
+  6, // Barbarian Hut
+  6, // Elixir Collector
+  6, // Giant Skeleton
+  6, // Lightning
+  6, // Goblin Giant
+  6, // X-Bow
+  6, // Sparky
+  6,  // Boss Bandit
+  7, //"Royal Recruits",
+  7, //  "Pekka",
+  7, //  "Electro Giant",
+  7, //  "Mega Knight",
+  7, //  "Lavahound",
+  8, //  "Golem",
+  9, //  "Three Musketeers"
 ];
+
+let rarities = [
+  1, // Skeletons
+  1, // Electro Spirit
+  1, // Fire Spirit
+  1, // Ice Spirit
+  2, // Heal Spirit
+  1, // Goblins
+  1, // Spear Goblins
+  1, // Bomber
+  1, // Bats
+  1, // Zap
+  1, // Giant Snowball
+  1, // Berserker
+  2, // Ice Golem
+  2, // Suspicious Bush
+  3, // Barbarian Barrel
+  3, // Wall Breakers
+  3, // Goblin Curse
+  3, // Rage
+  4,  // The Log
+  1, // Archers
+  1, // Arrows
+  1, // Knight
+  1, // Minions
+  1, // Cannon
+  1, // Goblin Gang
+  1, // Skeleton Barrel
+  1,  // Royal Delivery
+  2, // Tombstone
+  2, // Mega Minion
+  2, // Dart Goblin
+  2, // Earthquake
+  2, // Elixir Golem
+  3, // Goblin Barrel
+  3, // Guards
+  3, // Skeleton Army
+  3,  // Clone
+  3, // Tornado
+  3, // Void
+  4, // Miner
+  4, // Princess
+  4, // Ice Wizard
+  4, // Royal Ghost
+  4, // Bandit
+  4, // Fisherman
+  5,  // Little Prince
+  1, // Skeleton Dragons
+  1, // Mortar
+  1, // Tesla
+  2, // Fireball
+  2, // Magic Archer
+  2, // Musketeer
+  2, // Goblin Cage
+  2, // Valkyrie
+  2,  // Battle Ram
+  2, //"Bomb Tower",
+  2, //  "Flying Macine",
+  2,//    "Hog Rider",
+  2,//  "Battle Healer",
+  2,//    "Furnace",
+  2,//    "Zappies",
+  2,//    "Goblin Demolisher",
+  3,//    "Baby Dragon",
+  3,//    "Dark Prince",
+  3,//    "Freeze",
+  3,//    "Poison",
+  3,//    "Rune Giant",
+  3,//    "Hunter",
+  3,//    "Goblin Drill",
+  4,//    "Electro Wizard",
+  4,//    "Inferno Dragon",
+  4,//    "Phoenix",
+  4,//    "Magic Archer"
+  4,// Lumberjack
+  4,// Night Witch
+  4,// Mother Witch
+  5,// Golden Knight
+  5,// Skeleton King
+  5,// Mighty Miner
+  1, // Barbarians
+  1, // Minion Horde
+  1, // Rascals
+  2, // Giant
+  2, // Goblin Hut
+  2, // Inferno Tower
+  2, // Wizard
+  2, // Royal Hogs
+  3,  // Witch
+  3, // Balloon - Epic
+  3, // Prince - Epic
+  3, // Electro Dragon - Epic
+  3, // Bowler - Epic
+  3, // Executioner - Epic
+  3, // Cannon Cart - Epic
+  4, // Ram Rider
+  4, // Graveyard
+  4,  // Goblin Giant - Epic
+  5,
+  5,
+  5,
+  1, // Royal Giant - Common
+  1, // Elite Barbarians - Common
+  2, // Rocket - Rare
+  2, // Barbarian Hut - Rare
+  2, // Elixir Collector - Rare
+  3, // Giant Skeleton - Epic
+  3, // Lightning - Epic
+  3, // Goblin Giant - Epic
+  3, // X-Bow - Epic
+  4, // Sparky - Legendary
+  5,  // Boss Bandit - Hero
+  1, //"Royal Recruits",
+  3, //  "Pekka",
+  3, //  "Electro Giant",
+  4, //  "Mega Knight",
+  4, //  "Lavahound",
+  3, //  "Golem",
+  2, //  "Three Musketeers"
+
+];
+
+let sortedIndex = [];
+for(let i = 0; i<titles.length; i++)
+{
+  sortedIndex[i] = i;
+}
+
+function sortByRarity1()
+{
+  sortedIndex.sort((a, b) => {
+    return rarities[a] - rarities[b];
+  });
+}
+
+function sortByCost1()
+{
+  for(let i = 0; i<titles.length; i++)
+  {
+    sortedIndex[i] = i;
+  }
+}
 
 function showCards() {
   const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = ""; // Clear existing cards
+  cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
   for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-    let imageURL = URL[i];
+    let cardIndex = sortedIndex[i];
+    let title = titles[cardIndex];
+    let imageURL = URL[cardIndex];
 
-    const nextCard = templateCard.cloneNode(true); // Clone card template
-    editCardContent(nextCard, title, imageURL, i); // Edit content
-    cardContainer.appendChild(nextCard); // Append new card to container
+    const nextCard = templateCard.cloneNode(true);
+    nextCard.setAttribute("data-card-index", cardIndex);
+    nextCard.classList.add("clickable");
+
+    editCardContent(nextCard, title, imageURL, cardIndex);
+    cardContainer.appendChild(nextCard);
   }
+
+  initializeCardClicks();
+  initializeDeckClicks();
+  searchCards();
 }
 
 function editCardContent(card, newTitle, newImageURL, index) {
@@ -230,11 +667,101 @@ function editCardContent(card, newTitle, newImageURL, index) {
 
 document.addEventListener("DOMContentLoaded", showCards);
 
-function quoteAlert() {
-  alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
+function sortByRarity() {
+  sortByRarity1();
+  showCards();
 }
 
-function removeLastCard() {
-  titles.pop(); // Remove last card from titles array
-  showCards(); // Refresh cards
+function sortByCost() {
+  sortByCost1();
+  showCards();
+}
+
+let userDeck = [];
+
+function initializeCardClicks() {
+  const cards = document.querySelectorAll("#card-container .card");
+  cards.forEach(card => {
+    card.addEventListener("click", () => {
+      const index = parseInt(card.getAttribute("data-card-index"));
+      if (!userDeck.includes(index) && userDeck.length < 8) {
+        userDeck.push(index);
+        showUserDeck();
+      }
+    });
+  });
+}
+
+function initializeDeckClicks() {
+  const cards = document.querySelectorAll("#deck-container .card");
+  cards.forEach(card => {
+    card.addEventListener("click", () => {
+      const index = parseInt(card.getAttribute("data-card-index"));
+      userDeck = userDeck.filter(i => i !== index);
+      showUserDeck();
+    });
+  });
+}
+
+function searchCards() {
+  const searchQuery = document.getElementById("search-box").value.toLowerCase(); // Get search input and convert to lowercase
+  const cardContainer = document.getElementById("card-container");
+  const templateCard = document.querySelector(".card");
+  
+  cardContainer.innerHTML = ""; // Clear existing cards
+
+  // Filter the cards by title matching the search query
+  for (let i = 0; i < titles.length; i++) {
+    let title = titles[i].toLowerCase(); // Convert title to lowercase for case-insensitive comparison
+    if (title.includes(searchQuery)) {
+      let imageURL = URL[i];
+
+      const nextCard = templateCard.cloneNode(true);
+      nextCard.setAttribute("data-card-index", i);
+      nextCard.classList.add("clickable");
+
+      editCardContent(nextCard, titles[i], imageURL, i); // Update card content
+      cardContainer.appendChild(nextCard);
+    }
+  }
+}
+
+function showUserDeck() {
+  const deckContainer = document.getElementById("deck-container");
+  const deckInfo = document.getElementById("card-info"); // Make sure this matches your HTML
+  deckContainer.innerHTML = "";
+  deckInfo.innerHTML = ""; // Clear previous info
+
+  const templateCard = document.querySelector(".card");
+
+  let totalElixir = 0;
+
+  for (let i = 0; i < userDeck.length; i++) {
+    const index = userDeck[i];
+    let title = titles[index];
+    let imageURL = URL[index];
+    totalElixir += elixerCosts[index]; // Add to average elixir calculation
+
+    const nextCard = templateCard.cloneNode(true);
+    nextCard.setAttribute("data-card-index", index);
+    editCardContent(nextCard, title, imageURL, index);
+    deckContainer.appendChild(nextCard);
+  }
+
+  // Show average elixir and other info
+  if (userDeck.length > 0) {
+    const average = (totalElixir / userDeck.length).toFixed(1);
+    deckInfo.innerHTML = `
+      <div class="deck-info">
+        <h3>Deck Info</h3>
+        <p><strong>Cards:</strong> ${userDeck.length} / 8</p>
+        <p><strong>Average Elixir:</strong> ${average}</p>
+        <ul>
+          ${userDeck.map(i => `<li>${titles[i]}</li>`).join("")}
+        </ul>
+      </div>
+    `;
+  }
+
+  initializeDeckClicks();
 }
